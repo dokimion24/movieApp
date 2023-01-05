9a368d9cd30ce6f827b033eee3f00b9f85f3e1cf
Dokimion0 <leecy3610@gmail.com>
2023-01-05 17:16:13 +0900

    feat: 무한 스크롤 기능 추가

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import { getMovies } from './api.js';
 const $movies = document.querySelector('ul');
 const $moreBtn = document.querySelector('.more');
 const $searchForm = document.querySelector('form');
-const $input = $searchForm.querySelector('input');
-const $searchBtn = $searchForm.querySelector('button');
-const $searchText = document.querySelector('.search-result');
+const $searchInput = $searchForm.querySelector('input');
+const $searchText = document.querySelector('.text');
+const $searchTextResult = document.querySelector('.text-result');
 
-const moveInfo = {
+const movieInfo = {
   title: '',
   year: '',
   page: '',
@@ -16,26 +16,23 @@ const moveInfo = {
 $searchForm.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  moveInfo.title = $input.value;
-  moveInfo.page = 1;
+  movieInfo.title = $searchInput.value;
+  movieInfo.page = 1;
+  const movies = await getMovies(movieInfo);
 
-  const movies = await getMovies(moveInfo);
-
-  $searchText.innerHTML = `'${moveInfo.title}' 의 검색결과`;
-  $searchText.style.display = 'none';
+  $searchTextResult.innerHTML = `'${movieInfo.title}' 의 검색결과`;
+  $searchText.style.display = 'block';
 
   renderMovies(movies, true);
 });
 
-$moreBtn.addEventListener('click', async () => {
-  moveInfo.page += 1;
-  const movies = await getMovies(moveInfo);
-  if (moveInfo.page * 10 <= movies.totalResults) {
-    renderMovies(movies, false);
-  }
-});
-
-const movies = [];
+// $moreBtn.addEventListener('click', async () => {
+//   movieInfo.page += 1;
+//   const movies = await getMovies(movieInfo);
+//   if (moviInfo.page * 10 <= movies.totalResults) {
+//     renderMovies(movies, false);
+//   }
+// });
 
 const renderMovies = (movies, isFirst) => {
   if (isFirst) {
@@ -50,4 +47,26 @@ const renderMovies = (movies, isFirst) => {
     `;
     $movies.append($movie);
   });
+
+  // 무한 스크롤
+  (() => {
+    const options = { threshold: 0.2 };
+
+    const callback = (entries, observer) => {
+      entries.forEach(async (entry) => {
+        if (entry.isIntersecting) {
+          const movies = await getMovies(movieInfo);
+          if (movies.totalResults >= movieInfo.page * 10) {
+            renderMovies(movies, false);
+            movieInfo.page += 1;
+          } else {
+            io.unobserve(entry.target);
+          }
+        }
+      });
+    };
+
+    const io = new IntersectionObserver(callback, options);
+    io.observe(document.querySelector('li:last-child'));
+  })();
 };
