c8fbdb2d223d74f3591235df3a2c285212c4b442
Dokimion0 <leecy3610@gmail.com>
2023-01-05 13:59:00 +0900

    feat: 더보기 기능 완료

--- src/main.js
+++ src/main.js
@@ -1,56 +1,41 @@
+import { getMovies } from './api.js';
+
 const $movies = document.querySelector('ul');
 const $moreBtn = document.querySelector('.more');
 const $searchForm = document.querySelector('form');
 const $input = $searchForm.querySelector('input');
 const $searchBtn = $searchForm.querySelector('button');
+const $searchText = document.querySelector('.search-result');
 
-const search = {
+const moveInfo = {
   title: '',
   year: '',
   page: '',
 };
 
-// searchformEl.addEventListener('input', () => {
-//   searchText = inputEl.value;
-// });
-
 $searchForm.addEventListener('submit', async (e) => {
   e.preventDefault();
 
-  search.title = $input.value;
-  search.page = 1;
-  const movies = await getMovies();
-  console.log(movies);
-  console.log(search);
+  moveInfo.title = $input.value;
+  moveInfo.page = 1;
+
+  const movies = await getMovies(moveInfo);
+
+  $searchText.innerHTML = `'${moveInfo.title}' 의 검색결과`;
+  $searchText.style.display = 'none';
+
   renderMovies(movies, true);
 });
 
 $moreBtn.addEventListener('click', async () => {
-  search.page++;
-  const movies = await getMovies();
-  if (search.page * 10 <= movies.totalResults) {
+  moveInfo.page += 1;
+  const movies = await getMovies(moveInfo);
+  if (moveInfo.page * 10 <= movies.totalResults) {
     renderMovies(movies, false);
   }
 });
 
 const movies = [];
-
-// 검색 영화 가져오기
-const getMovies = async () => {
-  const s = `&s=${search.title}`;
-  const y = `&y=${search.year}`;
-  const p = `&page=${search.page}`;
-  const res = await fetch(`https://omdbapi.com/?apikey=7035c60c${s}${y}${p}`);
-  const json = await res.json();
-  if (json.Response === 'True') {
-    const { Search, totalResults } = json;
-    return {
-      Search,
-      totalResults,
-    };
-  }
-  return json.Error;
-};
 
 const renderMovies = (movies, isFirst) => {
   if (isFirst) {
