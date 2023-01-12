83a356e7217eb9eeae95cc4ee0b0d3279603eb0b
Dokimion0 <leecy3610@gmail.com>
2023-01-12 10:55:37 +0900

    chore: modal.js 생성
    모달창 관련 함수 추가
    1. startLoading()
    2. stopLoading()
    3. toggleBackdrop()
    4. toggleScrolling()
    5. toggleModal()
    6. clickCancleBtn()

---
+++ src/modal.js
@@ -0,0 +1,30 @@
+const $modal = document.querySelector('.modal');
+const $body = document.querySelector('body');
+const $loaderWrap = document.querySelector('.loader-wrap');
+
+const startLoading = () => {
+  $loaderWrap.classList.add('visible');
+};
+
+export const stopLoading = () => {
+  $loaderWrap.classList.remove('visible');
+};
+
+const toggleBackdrop = () => {
+  $modal.classList.toggle('backdrop');
+};
+
+const toggleScrolling = () => {
+  $body.classList.toggle('stop-scrolling');
+};
+
+export const toggleModal = () => {
+  $modal.classList.toggle('visible');
+  startLoading();
+  toggleBackdrop();
+  toggleScrolling();
+};
+
+export const clickCancleBtn = () => {
+  toggleModal();
+};
